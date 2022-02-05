c63365d041fd68d121c6fdf36ed9e50e9009f1ab
Mohamed Dhik <92504802+mohamedhik@users.noreply.github.com>
2022-02-05 22:03:18 +0100

    Update and rename request-handler.go to 1 - request-handler.go

--- 1 - request-handler.go
+++ 1 - request-handler.go
@@ -6,9 +6,14 @@ import (
 )
 
 func main() {
+	
+	// Registering a Request Handler
+	
 	http.HandleFunc("/Dhik", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Hello you've requested: %s\n", r.URL.Path)
 	})
-
+	
+	// Setting up an HTTP listner on port 8080
+	
 	http.ListenAndServe(":8080", nil)
 }
